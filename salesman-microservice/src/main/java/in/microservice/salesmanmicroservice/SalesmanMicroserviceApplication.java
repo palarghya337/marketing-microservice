82b41f6bbed397b7138127d9b322874080e3433b
Arghya Pal <palarghya337@gmail.com>
2020-04-18 12:40:32 +0530

    forgot to add application name and EnableEurekaClient annotation in the previous commit

--- salesman-microservice/src/main/java/in/microservice/salesmanmicroservice/SalesmanMicroserviceApplication.java
+++ salesman-microservice/src/main/java/in/microservice/salesmanmicroservice/SalesmanMicroserviceApplication.java
@@ -2,8 +2,10 @@ package in.microservice.salesmanmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SalesmanMicroserviceApplication {
 
 	public static void main(String[] args) {
